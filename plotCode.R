44af8786ed09e80ec9a96e199d9e4090ec9801c6
Maura <mauranish1112@gmail.com>
2025-01-05 12:21:09 -0500

    Draft of Homework 2
    Create a bivariate plot from one numeric and one categorical variable and add it to the dashboard.

--- plotCode.R
+++ plotCode.R
@@ -1,3 +1,11 @@
+
+# import necessary libraries
+library(tidyverse)
+library(ggplot2)
+library(rio)
+
+################################## PLOT 1 ##################################
+
 # load in the washington school data
 load(file=url("https://github.com/DACSS-Visual/tabular_univar_cat/raw/main/data/eduwa.rda"))
 
@@ -8,7 +16,6 @@ summary(eduwa$Student.Teacher.Ratio)
 # min = 0.1, Q1 = 16.2, median = 18.3, mean = 18.95, Q3 = 20.2, max = 330, NAs = 329
 
 # modify data to remove highest 2 outliers and 329 NAs
-library(tidyverse)
 eduwa_new <- filter(eduwa, Student.Teacher.Ratio<100)
 nrow(eduwa_new)
 # new data only has 2096 rows -- 331 were removed
@@ -35,8 +42,6 @@ sum(eduwa_new$Student.Teacher.Ratio <= 14.4)
 # only 13.3% of schools are less than or equal to the 2022 national average
 
 # create plot
-library(ggplot2)
-# specify dataset and variable to plot
 base <- ggplot(data=eduwa_new, aes(x=Student.Teacher.Ratio))
 # use a boxplot
 basebox <- base + geom_boxplot()
@@ -73,3 +78,84 @@ uniplot
 
 # save plot
 saveRDS(uniplot, file="uniplot.rds")
+
+################################## PLOT 2 ##################################
+
+# load in MA state police arrests data
+ma_arrests <- rio::import("https://github.com/DACSS-Visual/tabular_bivar_catcat/raw/refs/heads/main/data/MSP%20DFS%20Arrests%2019-20Q1.xlsx",
+                          which = 1)
+
+# explore variables of choice 
+# Arrest Type
+class(ma_arrests$`Arrest Type`)
+# Arrest Type is a character variable with 4 unique values, so convert to factor
+ma_arrests <- mutate(ma_arrests, ArrestType = factor(x=`Arrest Type`, 
+                                                     levels = c('M', 'W', 'F', 'O'),
+                                                     labels = c('Misdemeanor', 'Warrant',
+                                                                'Felony', 'Other')))
+table(ma_arrests$ArrestType)
+# Misdemeanor     Warrant      Felony       Other 
+#    5441          1706         1251         180 
+# M = Misdemeanor (less serious crime)
+# W = Warrant (arrest with permission of judge)
+# F = Felony (more serious crime)
+# O = Other
+sum(is.na(ma_arrests$ArrestType))
+# 44 NA values
+ggplot(ma_arrests, aes(x=ArrestType)) + geom_bar()
+# many more misdemeanors than any other type
+
+# Age
+class(ma_arrests$Age)
+# Age is a numeric variable ranging from 15 to 74
+table(ma_arrests$Age)
+summary(ma_arrests$Age)
+#   Min.  1st Qu.  Median  Mean   3rd Qu.   Max.    NA's 
+#  15.00   27.00   33.00   34.89   41.00   74.00     5 
+hist(ma_arrests$Age)
+# majority of ages fall between 20 and 40
+
+# Arrest Type and Age
+# remove NA values in both variables of interest
+ma_arrests <- filter(ma_arrests, !is.na(ma_arrests$ArrestType) & 
+                       !is.na(ma_arrests$Age))
+tapply(ma_arrests$Age, ma_arrests$ArrestType, summary)
+# $Misdemeanor (n=5441)
+#  Min.  1st Qu.  Median  Mean   3rd Qu.   Max. 
+# 15.00   27.00   34.00   35.28   41.00   74.00 
+# $Warrant (n=1706)
+#  Min.  1st Qu.  Median  Mean   3rd Qu.   Max. 
+# 17.00   27.00   33.00   34.94   41.00   74.00 
+# $Felony (n=1251)
+#  Min.  1st Qu.  Median  Mean   3rd Qu.   Max. 
+# 15.00   25.00   30.00   32.71   39.00   71.00 
+# $Other (n=180)
+#  Min.  1st Qu.  Median  Mean   3rd Qu.   Max. 
+# 19.00   28.00   35.50   37.68   46.00   74.00 
+# at a glance, the distributions of age look similar for each category
+
+# create plot
+base <- ggplot(data = ma_arrests, aes(x=reorder(ArrestType, desc(ArrestType)), y=Age))
+# use a side-by-side boxplot with flipped axes and minimal theme
+basebox <- base + geom_boxplot() + coord_flip() + theme_minimal()
+# modify x and y axes
+xyaxes <- basebox + scale_y_continuous(breaks = c(20, 30, 40, 50, 60, 70)) +
+  labs(y = "Age (years)", x = NULL)
+# add title, subtitle, and caption
+titlescap <- xyaxes + 
+  labs(title = "Felonies Concerningly Committed by Younger Demographic than Other Types of Crimes", 
+       subtitle = "Arrests made by Massachusetts State Police between 01/01/2019 and 03/28/2020",
+       caption = "Not Included: 49 Arrests With No Recorded Type or Age\nSource: Mass.gov") +
+  theme(plot.title = element_text(size=9),
+        plot.subtitle = element_text(size=8))
+# add annotation for number of crimes of each type
+biplot <- titlescap + 
+  annotate(geom = 'text', label = "n = 5441", y = 18, x = 4.15, angle = 0) +
+  annotate(geom = 'text', label = "n = 1706", y = 18, x = 3.15, angle = 0) +
+  annotate(geom = 'text', label = "n = 1251", y = 18, x = 2.15, angle = 0) +
+  annotate(geom = 'text', label = "n = 180", y = 18, x = 1.15, angle = 0)
+# view final plot
+biplot
+
+# save plot
+saveRDS(biplot, file="biplot.rds")
